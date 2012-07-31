1d87d32167efabd256c14b7768dab5378828f55b
jwalter <joakim.waltersson@gmail.com>
2012-07-31 09:29:03 +0200

    The application runner - AuctionSniperDriver (Fest version)

---
+++ src/com/waltersson/sniper/AuctionSniperDriver.java
@@ -0,0 +1,22 @@
+package com.waltersson.sniper;
+
+import org.fest.swing.core.BasicRobot;
+import org.fest.swing.core.Robot;
+import org.fest.swing.finder.WindowFinder;
+import org.fest.swing.fixture.FrameFixture;
+
+public class AuctionSniperDriver
+{
+
+  private FrameFixture frame;
+
+  public AuctionSniperDriver(int timeout, Robot robot)
+  {
+    frame = WindowFinder.findFrame(Main.MAIN_WINDOW_NAME).withTimeout(timeout).using(robot);
+  }
+  
+  public void showsSniperStatus(String statusText) {
+    frame.label("").requireText(statusText);
+  }
+
+}
