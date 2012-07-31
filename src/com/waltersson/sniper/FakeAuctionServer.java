0a7f6817ef0c8cd37517267515460c31e2268ba9
jwalter <joakim.waltersson@gmail.com>
2012-07-31 09:33:40 +0200

    The fake auction

---
+++ src/com/waltersson/sniper/FakeAuctionServer.java
@@ -0,0 +1,45 @@
+package com.waltersson.sniper;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ChatManagerListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+
+public class FakeAuctionServer
+{
+  public static final String ITEM_ID_AS_LOGIN = "auction-%s";
+
+  public static final String AUCTION_RESOURCE = "Auction";
+
+  public static final String XMPP_HOSTNAME = "localhost";
+
+  private static final String AUCTION_PASSWORD = "auction";
+
+  private final String itemId;
+
+  private final XMPPConnection connection;
+
+  private Chat currentChat;
+
+  public FakeAuctionServer(String itemId)
+  {
+    this.itemId = itemId;
+    this.connection = new XMPPConnection(XMPP_HOSTNAME);
+  }
+
+  public void startSellingItem() throws XMPPException {
+    connection.connect();
+    connection.login(String.format(ITEM_ID_AS_LOGIN, itemId), AUCTION_PASSWORD, AUCTION_RESOURCE);
+    connection.getChatManager().addChatListener(
+      new ChatManagerListener() {
+          
+          @Override public void chatCreated(Chat chat, boolean createdLocally) {
+            currentChat = chat;
+          }
+      });
+  }
+  
+  public String getItemId() {
+    return itemId;
+  }
+}
